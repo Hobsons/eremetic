951cc208e1a79ed7fa58c0765d33df0b88560bda
Matt Demaray <matt.demaray@hobsons.com>
2016-04-09 14:51:51 -0700

    more info out of marathon.sh

--- docker/marathon.sh
+++ docker/marathon.sh
@@ -4,8 +4,14 @@ lookup_host() {
     nslookup "$1" | awk -v HOST="$1" '{ if ($2 == HOST) { getline; gsub(/^.*: /, ""); print $1 } }'
 }
 
+echo "HOST $HOST"
+echo "DOMAIN $DOMAIN"
+echo "PORT1 $PORT1"
+
 export MESSENGER_ADDRESS=`lookup_host ${HOST}${DOMAIN:+.$DOMAIN}`
 export MESSENGER_PORT=$PORT1
 
+echo "MESSENGER_ADDRESS $MESSENGER_ADDRESS"
+echo "MESSENGER_PORT $MESSENGER_PORT"
 
 exec /go/bin/eremetic
