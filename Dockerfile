bd94054ed8633dd94983a432bae0c06a1bb30981
Matt Demaray <matt.demaray@hobsons.com>
2016-04-10 11:49:43 -0700

    more debugging

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,6 @@ RUN apk add --no-cache --virtual git
 ADD . /go/src/github.com/Hobsons/eremetic
 WORKDIR /go/src/github.com/Hobsons/eremetic
 RUN go get -v -d
+RUN cp scheduler.go /go/src/github.com/mesos/mesos-go/scheduler/scheduler.go
 RUN go install -v github.com/Hobsons/eremetic
 CMD [ "docker/marathon.sh" ]
--- docker/marathon.sh
+++ docker/marathon.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
 
 lookup_host() {
+	echo "NSLOOKUP_1 $1" >&2
     nslookup "$1" | awk -v HOST="$1" '{ if ($2 == HOST) { getline; gsub(/^.*: /, ""); print $1 } }'
 }
 
-echo "HOST $HOST"
-echo "DOMAIN $DOMAIN"
-echo "PORT1 $PORT1"
+echo "HOST $HOST" >&2
+echo "DOMAIN $DOMAIN" >&2
+echo "PORT1 $PORT1" >&2
 
 export MESSENGER_ADDRESS=`lookup_host ${HOST}${DOMAIN:+.$DOMAIN}`
 export MESSENGER_PORT=$PORT1
 
-echo "MESSENGER_ADDRESS $MESSENGER_ADDRESS"
-echo "MESSENGER_PORT $MESSENGER_PORT"
+echo "MESSENGER_ADDRESS $MESSENGER_ADDRESS" >&2
+echo "MESSENGER_PORT $MESSENGER_PORT" >&2
 
 exec /go/bin/eremetic
